0714a240721962dd3b42cd74eb42dfb2a995715c
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-01-19 12:08:41 +0900

    Time: 39 ms (77.36%), Space: 14.3 MB (16.10%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        maps = {}
+
+        for i in range(len(nums)):
+            if nums[i] in maps:
+                return [i, maps[nums[i]]]
+            comp = target - nums[i]
+            maps[comp] = i
+            
