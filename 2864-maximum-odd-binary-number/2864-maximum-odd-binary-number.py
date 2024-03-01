91a661f5080e2c63fc45deda30f9111c5d3f9053
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-03-01 14:20:28 +0900

    Time: 22 ms (30.88%), Space: 11.7 MB (72.06%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximumOddBinaryNumber(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return '1'*(s.count('1')-1)+'0'*s.count('0')+'1'
+        
