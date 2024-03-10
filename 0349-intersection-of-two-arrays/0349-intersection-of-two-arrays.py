0f2a170292f4257f6ac27cdfc90fbc494f6b1e84
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-03-10 17:10:39 +0900

    Time: 37 ms (25.20%), Space: 11.9 MB (24.24%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -5,18 +5,7 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1.sort()
-        nums2.sort()
-        l = r = 0
-        n, m = len(nums1), len(nums2)
-        ans = set()
-        while l < n and r < m:
-            if nums1[l] == nums2[r]:
-                ans.add(nums1[l])
-                l += 1
-                r += 1
-            elif nums1[l] < nums2[r]:
-                l += 1
-            else:
-                r += 1
-        return list(ans)
+        cnt1 = Counter(nums1)
+        cnt2 = Counter(nums2)
+
+        return (cnt1&cnt2)
