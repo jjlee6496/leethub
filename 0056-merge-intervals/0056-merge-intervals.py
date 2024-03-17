39591287d3da7465e21beffd0c27c63a18a6bb57
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-03-17 15:35:02 +0900

    Time: 110 ms (20.25%), Space: 16.7 MB (78.18%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals = sorted(intervals, key = lambda x: x[0])
+        res = []
+
+        for i in intervals:
+            if not res or res[-1][1] < i[0]:
+                res.append(i)
+            else:
+                res[-1][1] = max(res[-1][1], i[1])
+        return res
