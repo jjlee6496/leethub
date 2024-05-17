472dc1ae2c42610678248b1d01ea078f07c8634e
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-05-17 16:50:27 +0900

    Time: 219 ms (79.11%), Space: 15.2 MB (42.16%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -5,12 +5,23 @@ class Solution(object):
         :rtype: int
         """
         N = len(grid)
-        dp = grid[0]
+
+        # 각 row의 최소값 2개만 필요
+        def get_min_two(r):
+            m1, m2 = float('inf'), float('inf')
+            idx1, idx2 = -1, -1
+            for c in range(N):
+                if grid[r][c] <= m1:
+                    m1, m2 = grid[r][c], m1
+                    idx1, idx2 = c, idx1
+                elif grid[r][c] < m2:
+                    m2, idx2 = grid[r][c], c
+            return [(m1, idx1), (m2, idx2)]
+        # 각 row에서 찾아온 최솟값 2개로 업데이트
+        # O(N^2), O(1)
         for r in range(1, N):
-            next_dp = [float('inf')]*N
-            for curr_c in range(N):
-                for prev_c in range(N):
-                    if prev_c != curr_c:
-                        next_dp[curr_c] = min(next_dp[curr_c], dp[prev_c]+grid[r][curr_c])
-            dp = next_dp
-        return min(dp)
+            min_dp = get_min_two(r-1)
+            for c in range(N):
+                grid[r][c] += min_dp[0][0] if min_dp[0][1] != c else min_dp[1][0]
+
+        return min(grid[N-1])
