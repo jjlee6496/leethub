a58ff25fd989bb15d1e28fafaac47b68970909a4
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-05-31 15:50:30 +0900

    Time: 20 ms (49.85%), Space: 11.9 MB (53.73%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
         res = []
         def dfs(ind, path):
-            if tuple(sorted(path[:])) not in res:
-                res.append(tuple(sorted(path[:])))
+            res.append(path[:])
+
             for i in range(ind, len(nums)):
+                if i > ind and nums[i-1] == nums[i]:
+                    continue
                 path.append(nums[i])
                 dfs(i+1, path)
                 path.pop()
