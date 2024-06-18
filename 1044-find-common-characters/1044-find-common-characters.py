7c2042284f15328f590068a4cdcdb2680e871dcc
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-06-18 16:05:30 +0900

    Time: 21 ms (95.56%), Space: 11.9 MB (50.62%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.py
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -5,18 +5,7 @@ class Solution(object):
         :rtype: List[str]
         """
         res = []
-        cnt = [0 for _ in range(26)]
-        for i in words[0]:
-            cnt[ord(i) - ord('a')] += 1
-        
-        for word in words[1:]:
-            temp = [0 for _ in range(26)]
-            for j in word:
-                temp[ord(j) - ord('a')] += 1
-            for k in range(26):
-                cnt[k] = min(cnt[k], temp[k])
-        for i in range(26):
-            if cnt[i]:
-                for _ in range(cnt[i]):
-                    res.append(chr(i + ord('a')))
+        for c in set(words[0]):
+            cnt = min([x.count(c) for x in words])
+            res.extend([c]*cnt)
         return res
