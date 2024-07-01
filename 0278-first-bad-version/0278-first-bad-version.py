6be2847b3df80753bc8a3283b1c9eb4e4b5a9c94
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-01 18:28:33 +0900

    Time: 19 ms (14.21%), Space: 11.4 MB (84.86%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -9,13 +9,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        l, r = 0, n
-        while isBadVersion(r):
+        l, r = 1, n
+        while l < r:
             mid = (l + r) // 2
 
             if isBadVersion(mid):
-                r = mid - 1
+                r = mid
             else:
                 l = mid + 1
 
-        return r+1
+        return r
