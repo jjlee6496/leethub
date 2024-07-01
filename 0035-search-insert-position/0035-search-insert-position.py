5f1ce6900cf3760937956083caad8526df0159cb
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-01 19:12:24 +0900

    Time: 26 ms (79.31%), Space: 12.2 MB (79.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,21 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
-        if target <= nums[0]:
-            return 0
-
         if target > nums[-1]:
             return len(nums)
-
+        if target <= nums[0]:
+            return 0
         l, r = 0, len(nums)-1
         while l <= r:
             mid = (l + r) // 2
-
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
+            elif nums[mid] > target:
                 r = mid - 1
-        return mid if nums[mid] > target else mid + 1
+            else:
+                l = mid + 1
+        return mid+1 if nums[mid] < target else mid
