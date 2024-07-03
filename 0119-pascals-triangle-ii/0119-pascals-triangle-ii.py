baab207a22dad8c4c950a19ca62f8f1a02b00e89
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-03 17:47:33 +0900

    Time: 16 ms (47.43%), Space: 11.5 MB (90.29%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        if rowIndex < 2:
-            return [1]*(rowIndex+1)
-        mem = [[1,1], []]
-        for i in range(1, rowIndex):
-            mem[1] = [1]
-            for j in range(1, i+1):
-                mem[1].append(mem[0][j]+mem[0][j-1])
-            mem[1].append(1)
-            mem[0] = mem[1]
-        return mem[0]
+
+        res = [1]
+        prev = 1
+        for k in range(1, rowIndex + 1):
+            next = prev * (rowIndex - k + 1) / k
+            res.append(next)
+            prev = next
+        return res
