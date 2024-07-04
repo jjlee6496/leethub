c1891078b52c985139b53c1eff82f01adc1a5bd8
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-04 12:57:33 +0900

    Time: 254 ms (100%), Space: 20.5 MB (14.81%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -7,10 +7,15 @@ class Solution(object):
         if len(nums) < 5:
             return 0
 
-        nums.sort()
+        # nums.sort()
         res = float('inf')
 
-        for l in range(4):
-            r = len(nums) - 4 + l # 3,2,1,0
-            res = min(res, nums[r] - nums[l])
+        # for l in range(4):
+        #     r = len(nums) - 4 + l # 3,2,1,0
+        #     res = min(res, nums[r] - nums[l])
+        # return res
+        maxs = sorted(heapq.nlargest(4, nums))
+        mins = sorted(heapq.nsmallest(4, nums))
+        for i in range(4):
+            res = min(res, maxs[i] - mins[i])
         return res
