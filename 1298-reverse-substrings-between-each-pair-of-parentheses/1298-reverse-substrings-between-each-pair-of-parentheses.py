8fc405a73880f9547a5d48ebb1a5e8d500e58868
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-16 16:12:13 +0900

    Time: 7 ms (97.81%), Space: 11.6 MB (95.18%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
@@ -6,15 +6,25 @@ class Solution(object):
         """
         stack = []
 
+        # for c in s:
+        #     if c == '(':
+        #         stack.append('(')
+        #     elif c == ')':
+        #         rev = []
+        #         while stack and stack[-1] != '(':
+        #             rev.append(stack.pop()[::-1])
+        #         stack.pop()
+        #         stack.append(''.join(rev))
+        #     else:
+        #         stack.append(c)
+        # return ''.join(stack)
         for c in s:
-            if c == '(':
-                stack.append('(')
-            elif c == ')':
-                rev = []
-                while stack and stack[-1] != '(':
-                    rev.append(stack.pop()[::-1])
+            if c == ')':
+                temp = []
+                while stack[-1] != '(':
+                    temp.append(stack.pop())
                 stack.pop()
-                stack.append(''.join(rev))
+                stack.extend(temp)
             else:
                 stack.append(c)
         return ''.join(stack)
