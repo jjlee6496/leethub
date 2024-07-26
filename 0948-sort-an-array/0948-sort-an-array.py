d01548a36c8820f63276523997c0a264b234d483
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-26 16:21:35 +0900

    Time: 594 ms (86.73%), Space: 25.8 MB (11.89%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        bucket = [0] * 100001
-        for x in nums:
-            bucket[x+50000] += 1
-        ans = []
-        for i, x in enumerate(bucket, -50000):
-            ans.extend([i] * x)
-        return ans
+        minV = min(nums)
+        maxV = max(nums)
+
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+        res = []
+        for i in range(minV, maxV+1):
+            if i not in dic:
+                continue
+            res.extend([i] * dic[i])
+        return res
+            
