5e7c4bd78405e2b5aeb5e8e58f3070c8b33a4c5a
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-26 16:12:59 +0900

    Time: 761 ms (82.48%), Space: 22.5 MB (14.54%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -4,23 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums) == 1:
-            return nums
-        
-        mid = len(nums) // 2
-        left = nums[:mid]
-        right = nums[mid:]
-
-        return self.merge(self.sortArray(left), self.sortArray(right))
-
-    def merge(self, left, right):
-        sorted_arr = []
-        while left and right:
-            if left[0] > right[0]:
-                sorted_arr.append(right.pop(0))
-            else:
-                sorted_arr.append(left.pop(0))
-        
-        return sorted_arr + left + right
-
-
+        bucket = [0] * 100001
+        for x in nums:
+            bucket[x+50000] += 1
+        ans = []
+        for i, x in enumerate(bucket, -50000):
+            ans.extend([i] * x)
+        return ans
