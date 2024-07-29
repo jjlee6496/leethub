a04793f7475b2c22dede1d0e8488b3e2c514f4b0
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-07-29 17:12:49 +0900

    Time: 659 ms (80%), Space: 11.9 MB (13.33%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.py
+++ 1511-count-number-of-teams/1511-count-number-of-teams.py
@@ -5,18 +5,11 @@ class Solution(object):
         :rtype: int
         """
         total = 0
-        for i in range(len(rating)):
-            l_s,  r_g,  l_g,  r_s = 0, 0, 0, 0
-            for j in range(0, i):
-                if rating[j] < rating[i]:
-                    l_s += 1
-                else:
-                    l_g += 1
-
-            for k in range(i+1, len(rating)):
-                if rating[k] < rating[i]:
-                    r_s += 1
-                else:
-                    r_g += 1
-            total += l_s * r_g + l_g * r_s
+        n = len(rating)
+        for i in range(1, n-1):
+            l_s = sum([rating[j] < rating[i] for j in range(i)])
+            l_g = i - l_s
+            r_g = sum([rating[k] > rating[i] for k in range(i+1, n)])
+            r_s = n - i - 1 - r_g
+            total += l_s*r_g + l_g*r_s
         return total
