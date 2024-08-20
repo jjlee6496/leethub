a559d0de89a752d2d0d300663e7d74eb38c52c3f
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-20 10:39:39 +0900

    Time: 19 ms (96.67%), Space: 11.7 MB (33.14%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -7,16 +7,17 @@ class Solution(object):
         """
         candidates.sort()
         res = []
-        def dfs(start, curr, path):            
+        def dfs(start, curr, path):
             if curr == target:
                 res.append(path[:])
-                return
             
             for i in range(start, len(candidates)):
-                if i > start and candidates[i-1] == candidates[i]: # 중복 제거
+                if i > start and candidates[i-1] == candidates[i]:
                     continue
-                if curr + candidates[i] > target: # 더 진행해봤자 의미 없으므로 종료
+                if candidates[i] + curr > target:
                     break
-                dfs(i + 1, curr + candidates[i], path + [candidates[i]])
+                path.append(candidates[i])
+                dfs(i + 1, candidates[i] + curr, path)
+                path.pop()
         dfs(0, 0, [])
         return res
