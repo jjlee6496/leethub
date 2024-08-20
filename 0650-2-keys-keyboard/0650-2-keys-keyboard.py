895005920124ba5538ace71c0b24dd28a274e019
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-20 12:39:42 +0900

    Time: 3 ms (100%), Space: 11.6 MB (58.21%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return 0
-        dp = [1001] * (n + 1)
-        dp[1] = 0
-        
-        for i in range(2, n+1):
-            for j in range(1, i // 2 + 1):
-                if i % j == 0:
-                    dp[i] = min(dp[i], dp[j] + i // j)
-        return dp[n]
+        for i in range(2, int(math.sqrt(n))+1):
+            if n % i == 0:
+                return self.minSteps(n / i) + i
+            
+        return 0 if n == 1 else n
