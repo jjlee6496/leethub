6358404dbb4d80aa0de981e94dbe1e32caa7046f
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-20 12:31:35 +0900

    Time: 283 ms (19.4%), Space: 11.6 MB (79.1%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        mem = {}
-        def helper(cnt, clip):
-            if cnt > n:
-                return 1001
-            if cnt == n:
-                return 0
-            if (cnt, clip) in mem:
-                return mem[(cnt, clip)]
-            res1 = 1 + helper(cnt + clip, clip)
-            res2 = 2 + helper(cnt + cnt, cnt)
-            mem[(cnt, clip)] = min(res1, res2)
-            return mem[(cnt, clip)]
         if n == 1:
             return 0
-        return 1 + helper(1, 1)
+        dp = [1001] * (n + 1)
+        dp[1] = 0
+        
+        for i in range(2, n+1):
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = min(dp[i], dp[j] + i // j)
+        return dp[n]
