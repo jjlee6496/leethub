c4d406e430ccabb01b4ad407ddcd66ce210a5611
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-22 14:51:17 +0900

    Time: 70 ms (78.88%), Space: 11.8 MB (39.6%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -4,17 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        minHeap = [1]
-        visited = set()
-        factors = [2, 3, 5]
-        for i in range(n):
-            num = heapq.heappop(minHeap)
-            
-            if i == n - 1:
-                return num
+        dp = [1]
+        i2, i3, i5 = 0, 0, 0
 
-            for f in factors:
-                if num * f not in visited:
-                    visited.add(num * f)
-                    heapq.heappush(minHeap, num * f)
+        for _ in range(1, n):
+            next_num = min(
+                dp[i2] * 2,
+                dp[i3] * 3,
+                dp[i5] * 5
+            )
+            dp.append(next_num)
 
+            if next_num == dp[i2] * 2:
+                i2 += 1
+            if next_num == dp[i3] * 3:
+                i3 += 1
+            if next_num == dp[i5] * 5:
+                i5 += 1
+        return dp[n - 1]
