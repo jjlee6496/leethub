c2791eb01cc2578ebc5c64d7fe20be0f4183794a
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-22 14:52:36 +0900

    Time: 1049 ms (5.04%), Space: 11.8 MB (58.98%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -4,21 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [1]
-        i2, i3, i5 = 0, 0, 0
-
-        for _ in range(1, n):
-            next_num = min(
-                dp[i2] * 2,
-                dp[i3] * 3,
-                dp[i5] * 5
-            )
-            dp.append(next_num)
-
-            if next_num == dp[i2] * 2:
-                i2 += 1
-            if next_num == dp[i3] * 3:
-                i3 += 1
-            if next_num == dp[i5] * 5:
-                i5 += 1
-        return dp[n - 1]
+        if n < 4:
+            return n
+        
+        ugly = sorted(2**a * 3 ** b * 5** c for a in range(32) for b in range(20) for c in range(14))
+        return ugly[n - 1]
