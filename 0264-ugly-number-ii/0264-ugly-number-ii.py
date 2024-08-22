ca9b94fa416b13e22f92c5f0f19455cb3218339f
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-22 14:52:51 +0900

    Time: 1053 ms (5.04%), Space: 12.1 MB (8.49%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n < 4:
-            return n
-        
+
         ugly = sorted(2**a * 3 ** b * 5** c for a in range(32) for b in range(20) for c in range(14))
         return ugly[n - 1]
