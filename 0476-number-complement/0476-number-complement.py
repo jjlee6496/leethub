7b0d6f3ae879879b8983db1f01188e03357c411a
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-22 12:35:50 +0900

    Time: 11 ms (78.13%), Space: 11.6 MB (27.81%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -4,4 +4,4 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        return int('0b'+''.join(['0' if b=='1' else '1' for b in bin(num)[2:]]), 2)
+        return int(''.join(['0' if b=='1' else '1' for b in bin(num)[2:]]), 2)
