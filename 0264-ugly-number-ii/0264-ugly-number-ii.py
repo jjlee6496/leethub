6c17ef450445ec59b9509251b8d3ace54ad94a9e
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-22 14:54:10 +0900

    Time: 10 ms (99.76%), Space: 11.9 MB (21.77%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,9 +1,9 @@
 class Solution(object):
+    ugly = sorted(2**a * 3 ** b * 5** c for a in range(32) for b in range(20) for c in range(14))
     def nthUglyNumber(self, n):
         """
         :type n: int
         :rtype: int
         """
 
-        ugly = sorted(2**a * 3 ** b * 5** c for a in range(32) for b in range(20) for c in range(14))
-        return ugly[n - 1]
+        return self.ugly[n - 1]
