2d30bfc0834fad3ea4abf86b61fb11b6928f3ff9
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-08-29 12:36:10 +0900

    Time: 447 ms (78.73%), Space: 11.9 MB (81.72%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -9,17 +9,16 @@ class Solution(object):
         m, n = len(grid), len(grid[0])
         q = deque([(0, 0, 1)])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-        visit = [[0]*n for _ in range(m)]
+        grid[0][0] = 1
     
         while q:
             r, c, s = q.popleft()
-            visit[r][c] = 1
             if r == m - 1 and c == n - 1:
                 return s
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if min(nr, nc) < 0 or nr == m or nc == n or grid[nr][nc] == 1 or visit[nr][nc] == 1:
+                if min(nr, nc) < 0 or nr == m or nc == n or grid[nr][nc] == 1:
                     continue
                 q.append((nr, nc , s + 1))
-                visit[nr][nc] = 1
+                grid[nr][nc] = 1
         return -1
