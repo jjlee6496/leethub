e61d6233822af85c4a02670a06130ee72f73f00f
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-04 17:44:06 +0900

    Time: 62 ms (86.86%), Space: 14.7 MB (44.73%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+        
+        order = []
+        visit = [0] * numCourses
+
+        def dfs(crs):
+            if visit[crs] == 2: # 방문함
+                return False
+            if visit[crs] == 1: # 방문중
+                return True
+            
+            visit[crs] = 1
+            for pre in graph[crs]:
+                if dfs(pre):
+                    return True
+            order.append(crs)
+            visit[crs] = 2
+            return False
+        
+        for i in range(numCourses):
+            if dfs(i):
+                return []
+        return order if order else []
