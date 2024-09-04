3773b831b60e20252b55e0fa957fe03329e5d781
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-04 16:01:48 +0900

    Time: 902 ms (75.68%), Space: 11.7 MB (89.19%) - LeetHub

--- 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -1,7 +1,7 @@
 class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.size = [1] * n
+        self.rank = [0] * n
 
     def find(self, x):
         if self.parent[x] != x:
@@ -12,10 +12,13 @@ class UnionFind:
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x != root_y:
-            if self.size[root_x] < self.size[root_y]:
-                root_x, root_y = root_y, root_x
-            self.size[root_x] += self.size[root_y]
-            self.parent[root_y] = root_x
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_x] = root_y
+                self.rank[root_y] += 1
             return True
         return False
 
@@ -31,21 +34,26 @@ class Solution(object):
 
         edges.sort(key=lambda x: x[2])  # sort by weight
 
-        def make_mst(to_include=-1, to_exclude=-1):
+        def make_mst(to_include=None, to_exclude=None):
             uf = UnionFind(n)
             weight = 0
-            if to_include != -1:
+            used = 0
+
+            if to_include is not None:
                 n1, n2, w, _ = edges[to_include]
                 uf.union(n1, n2)
                 weight += w
+                used += 1
             for i, (n1, n2, w, _) in enumerate(edges):
                 if i == to_exclude:
                     continue
                 if uf.union(n1, n2):
                     weight += w
-            # Check if all nodes are connected by counting unique roots
-            root_set = set(uf.find(x) for x in range(n))
-            return weight if len(root_set) == 1 else float('inf')
+                    used += 1
+                if used == n - 1:
+                    break
+
+            return weight if used == n - 1 else float('inf')
 
         critical, pseudo = [], []
         mst_weight = make_mst()
