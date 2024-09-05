98e80cebd99b7d3dbd79848fe7da959e5ddbf6b7
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-06 00:15:06 +0900

    Time: 2375 ms (50.22%), Space: 73.6 MB (48.48%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.py
+++ 2035-count-sub-islands/2035-count-sub-islands.py
@@ -6,11 +6,21 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid1), len(grid1[0])
-
         def dfs(i, j):
-            if 0 <= i < m and 0 <= j < n and grid2[i][j] == 1:
-                grid2[i][j] = 0
-                return grid1[i][j] & dfs(i+1, j) & dfs(i-1, j) & dfs(i, j+1) & dfs(i, j-1)
-            return 1
-
-        return sum(dfs(i, j) for i in range(m) for j in range(n) if grid2[i][j])
+            if min(i, j) < 0 or i == m or j == n or grid2[i][j] == 0:
+                return True
+            
+            grid2[i][j] = 0 # 방문
+            res = grid1[i][j] == 1
+            res &= dfs(i - 1, j)
+            res &= dfs(i + 1, j)
+            res &= dfs(i, j - 1)
+            res &= dfs(i, j + 1)
+            return res
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j] == 1 and dfs(i, j):
+                    res += 1
+        return res
+            
