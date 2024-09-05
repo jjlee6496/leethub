cedac467d01f7e72cc8fb7e3d2357dd8032e8245
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-05 18:42:48 +0900

    Time: 1193 ms (59.46%), Space: 11.8 MB (72.97%) - LeetHub

--- 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -9,16 +9,15 @@ class UnionFind:
         return self.parent[x]
 
     def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
+        root_x, root_y = self.find(x), self.find(y)
         if root_x != root_y:
-            if self.rank[root_x] < self.rank[root_y]:
-                self.parent[root_x] = root_y
-            elif self.rank[root_x] > self.rank[root_y]:
+            if self.rank[root_x] > self.rank[root_y]:
                 self.parent[root_y] = root_x
-            else:
+            elif self.rank[root_x] < self.rank[root_y]:
                 self.parent[root_x] = root_y
-                self.rank[root_y] += 1
+            else:
+                self.rank[root_x] += 1
+                self.parent[root_y] = root_x
             return True
         return False
 
@@ -30,40 +29,34 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         for i, e in enumerate(edges):
-            e.append(i)  # [u, v, w, i]
+            e.append(i) # (u, v, w, i)
+        edges.sort(key = lambda x: x[2]) # sort by weight
 
-        edges.sort(key=lambda x: x[2])  # sort by weight
-
-        def make_mst(to_include=None, to_exclude=None):
+        def make_mst(include=None, exclude=None):
             uf = UnionFind(n)
             weight = 0
             used = 0
-
-            if to_include is not None:
-                n1, n2, w, _ = edges[to_include]
+            if include is not None:
+                n1, n2, w, i = edges[include]
                 uf.union(n1, n2)
                 weight += w
                 used += 1
             for i, (n1, n2, w, _) in enumerate(edges):
-                if i == to_exclude:
+                if i == exclude:
                     continue
                 if uf.union(n1, n2):
                     weight += w
                     used += 1
                 if used == n - 1:
                     break
-
             return weight if used == n - 1 else float('inf')
-
-        critical, pseudo = [], []
+        
         mst_weight = make_mst()
-
+        critical, pseudo = [], []
         for i in range(len(edges)):
-            # Check if the edge is critical
-            if make_mst(to_exclude=i) > mst_weight:
+            w1, w2 = make_mst(exclude=i), make_mst(include=i)
+            if w1 > mst_weight:
                 critical.append(edges[i][3])
-            # Check if the edge is pseudo-critical
-            elif make_mst(to_include=i) == mst_weight:
+            elif w2 == mst_weight:
                 pseudo.append(edges[i][3])
-
         return [critical, pseudo]
