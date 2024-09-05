87e92df37097a9096eaeab60fe5f2e4bdc812e93
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-05 11:31:42 +0900

    Time: 12 ms (84.85%), Space: 11.9 MB (62.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -8,7 +8,7 @@ class Solution(object):
             return ""
         prefix = strs[0]
         for i in range(1, len(strs)):
-            while strs[i].find(prefix) != 0:
+            while strs[i][:len(prefix)] != prefix:
                 prefix = prefix[:-1]
                 if not prefix:
                     return ""
