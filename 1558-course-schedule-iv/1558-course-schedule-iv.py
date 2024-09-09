bce8c96ea2d047d2fcdece917a98eef3bc99f724
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-09 12:10:06 +0900

    Time: 1113 ms (19.79%), Space: 15.5 MB (72.92%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
+        graph = [[0]*numCourses for _ in range(numCourses)]
+        for pre, crs in prerequisites:
+            graph[pre][crs] = True
+        
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    if graph[i][k] and graph[k][j]:
+                        graph[i][j] = True
+        
+        res = []
+        for (u, v) in queries:
+            res.append(graph[u][v])
+        return res
