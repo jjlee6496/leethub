6b351e354a4697d357fd1246aa74bc5727a93bc5
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-18 18:44:40 +0900

    Time: 50 ms (14.39%), Space: 16.9 MB (32.99%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,8 +1,8 @@
-class compare(str):
-    def __lt__(a, b):
-        return a+b > b+a
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        class compare(str):
+            def __lt__(a, b):
+                return a+b > b+a
         nums = list(map(str, nums))
         nums.sort(key=compare)
         res = ''.join(nums)
