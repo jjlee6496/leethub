36bb39fd9c073ab8ad9265031bf9309ab19654e1
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-18 18:52:36 +0900

    Time: 16 ms (90.03%), Space: 11.7 MB (67.12%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,19 +1,7 @@
 class Solution(object):
-
     def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        def compare(a, b):
-            if a + b < b + a:
-                return 1
-            if a + b > b + a:
-                return -1
-            else:
-                return 0
-
-        nums = map(str, nums)
-        nums.sort(cmp=compare)
-        res = ''.join(nums)
-        return '0' if res[0] == '0' else res
+        nums = list(map(str, nums))
+        nums.sort(key=lambda x: x*10, reverse=True)
+        if nums[0] == '0':
+            return '0'
+        return ''.join(nums)
