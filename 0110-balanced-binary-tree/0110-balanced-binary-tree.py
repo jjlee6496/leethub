61b706443c14e4ba0a046b0468a3235b89b975a3
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-09-20 12:53:14 +0900

    Time: 27 ms (73.54%), Space: 16.7 MB (64.63%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -10,15 +10,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        def check(node):
-            if not node:
-                return 0
+        def valid(root):
+            if not root:
+                return 0, True
+            l, l_b = valid(root.left)
+            r, r_b = valid(root.right)
             
-            l, r = check(node.left), check(node.right)
-
-            if l < 0 or r < 0 or abs(l - r) > 1:
-                return -1
-            
-            return max(l, r) + 1
-
-        return check(root) >= 0
+            return 1 + max(l, r), l_b and r_b and abs(l - r) <= 1
+        
+        return valid(root)[1]
