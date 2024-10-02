0c69793682c5e6804820809845e5eabf428abbae
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-10-02 15:55:36 +0900

    Time: 257 ms (52.27%), Space: 28.8 MB (71.59%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        dic = {}
-        temp = sorted(arr)
-        i = 1
-        for num in temp:
-            if num in dic:
-                continue
-            dic[num] = i
-            i += 1
+        
+        temp = sorted(set(arr))
+        dic = {num:i for i, num in enumerate(temp, 1)}
         return [dic[num] for num in arr]
 
