6529c37ac3c721474150d68c5b085d5d47007284
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-10-11 09:27:05 +0900

    Time: 264 ms (88.81%), Space: 29.6 MB (68.3%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minSwaps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = 0
+        unbalanced = 0
+        for p in s:
+            if p == "[":
+                stack += 1
+            else:
+                if stack > 0:
+                    stack -= 1
+                else:
+                    unbalanced += 1
+        return (unbalanced + 1) // 2
