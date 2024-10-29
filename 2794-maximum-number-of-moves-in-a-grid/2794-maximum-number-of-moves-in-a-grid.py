058d7e6fe5bebaedb0c21b70cd01d3f8f0e72359
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-10-29 11:38:08 +0900

    Time: 516 ms (100%), Space: 19.2 MB (100%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.py
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.py
@@ -5,23 +5,10 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid), len(grid[0])
-        mem = {}
-        def move(i, j):
-            if (i, j) in mem:
-                return mem[(i, j)]
-            if min(i, j) < 0 or i >= m or j >= n:
-                return 0
-            max_steps = 0
-            if i + 1 < m and j + 1 < n and grid[i + 1][j + 1] > grid[i][j]:
-                max_steps = max(max_steps, move(i + 1, j + 1))
-            if i < m and j + 1 < n and grid[i][j + 1] > grid[i][j]:
-                max_steps = max(max_steps, move(i, j + 1))
-            if 0 <= i - 1 and j + 1 < n and grid[i - 1][j + 1] > grid[i][j]:
-                max_steps = max(max_steps, move(i - 1, j + 1))
-            
-            mem[(i, j)] = max_steps + 1
-            return max_steps + 1
-        res = 0
-        for i in range(m):
-            res = max(res, move(i, 0))
-        return res - 1
+        dp = [[0] * n for _ in range(m)]
+        for j in range(n - 2, -1, -1):
+            for i in range(m):
+                for ni in [i - 1, i, i + 1]:
+                    if 0 <= ni < m and grid[ni][j + 1] > grid[i][j]:
+                        dp[i][j] = max(dp[i][j], dp[ni][j + 1] + 1)
+        return max(dp[i][0] for i in range(m))
