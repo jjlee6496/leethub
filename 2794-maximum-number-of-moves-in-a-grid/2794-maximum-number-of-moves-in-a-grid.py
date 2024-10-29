43c96a3dc416e7bf00bc02d08d9ab3571d74a5ed
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-10-29 11:44:18 +0900

    Time: 461 ms (100%), Space: 18.3 MB (100%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.py
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.py
@@ -5,10 +5,12 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid), len(grid[0])
-        dp = [[0] * n for _ in range(m)]
+        curr = [0] * m
         for j in range(n - 2, -1, -1):
+            next_col = curr[:]
             for i in range(m):
+                curr[i] = 0
                 for ni in [i - 1, i, i + 1]:
                     if 0 <= ni < m and grid[ni][j + 1] > grid[i][j]:
-                        dp[i][j] = max(dp[i][j], dp[ni][j + 1] + 1)
-        return max(dp[i][0] for i in range(m))
+                        curr[i] = max(curr[i], next_col[ni] + 1)
+        return max(curr)
