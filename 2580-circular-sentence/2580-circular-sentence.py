54cd74bffae8a1aef339e243e2c0e0c6bc576f08
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-11-02 17:01:51 +0900

    Time: 3 ms (20%), Space: 11.7 MB (13.41%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.py
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -7,8 +7,6 @@ class Solution(object):
         sentence = sentence.split()
 
         n = len(sentence)
-        if n == 1:
-            return sentence[0][0] == sentence[0][-1]
 
         for i in range(1, n):
             if sentence[i - 1][-1] != sentence[i][0]:
