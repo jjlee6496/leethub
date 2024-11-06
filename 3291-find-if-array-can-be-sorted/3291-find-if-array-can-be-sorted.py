2948cd108f30bf4a9e81602bb06309105f38f2aa
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-11-06 16:12:55 +0900

    Time: 14 ms (80%), Space: 11.7 MB (38.46%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
@@ -4,20 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        changed = True
-        while changed:
-            changed = False
-            for i in range(len(nums) - 1):
-                if nums[i] > nums[i + 1]:
-                    if self.count_bits(nums[i]) == self.count_bits(nums[i + 1]):
-                        nums[i], nums[i + 1] = nums[i + 1], nums[i]
-                        changed = True
-                    else:
-                        return False
+        arr = []
+        temp = [nums[0]]
+        prev = self.count_bits(nums[0])
+        for i in range(1, len(nums)):
+            cnt = self.count_bits(nums[i])
+            if cnt == prev:
+                temp.append(nums[i])
+            else:
+                arr.append(temp)
+                temp = [nums[i]]
+                prev = cnt
+        arr.append(temp)
+        
+        for i in range(1, len(arr)):
+            if max(arr[i - 1]) > min(arr[i]):
+                return False
         return True
+
     def count_bits(self, num):
         cnt = 0
         while num:
-            cnt += num & 1
-            num >>= 1
+            num &= (num - 1)
+            cnt += 1
         return cnt
