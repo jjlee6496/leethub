2c7dce6fa7456540f2e0635a8adb35aeafa3096e
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-11-06 09:19:39 +0900

    Time: 27 ms (100%), Space: 13.5 MB (76.47%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -7,6 +7,6 @@ class Solution(object):
         res = 0
 
         for i in range(0, len(s), 2):
-            if (int(s[i]) + int(s[i+1])) % 2:
+            if s[i] != s[i + 1]:
                 res += 1
         return res
