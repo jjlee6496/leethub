c4cdc86264666d847ccb47ee8438e21fe473494b
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-11-11 12:07:36 +0900

    Time: 90 ms (100%), Space: 11.8 MB (33.33%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -8,9 +8,27 @@ class Solution(object):
         primes[0] = primes[1] = False
         for i in range(2, int(math.sqrt(limit)) + 1):
             if primes[i]:
-                for j in range(i+i, limit+1, i):
+                for j in range(i*i, limit+1, i):
                     primes[j] = False
         return [i for i in range(limit, 1, -1) if primes[i]]
+
+    def bs(self, target, prev):
+        l, r  = 0, len(self.primes) - 1
+        res = -1
+
+        while l <= r:
+            mid = (l + r) // 2
+            prime = self.primes[mid]
+
+            if prime >= target:
+                l = mid + 1
+            else:
+                if target - prime > prev:
+                    res = prime
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return res
 
     def primeSubOperation(self, nums):
         """
@@ -23,11 +41,12 @@ class Solution(object):
         
         prev = 0
         for i in range(n):
-            for p in self.primes:
-                if nums[i] > p and nums[i] - p > prev:
-                    nums[i] -= p
-                    break
+            prime = self.bs(nums[i], prev)
+            if prime != -1:
+                nums[i] -= prime
+
             if nums[i] <= prev:
                 return False
+            
             prev = nums[i]
         return True
