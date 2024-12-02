89d1d06b54c4f2cb23fae7bb9b3427e991fa3d4a
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2024-12-02 17:27:57 +0900

    Time: 0 ms (100%), Space: 12.1 MB (7.69%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        arr.sort()
-        l = 0
-        for r in range(len(arr)):
-            if arr[l] * 2 > arr[r]:
-                continue
-            temp = l
-            while l < r:
-                if arr[l] * 2 == arr[r] or arr[r] * 2 == arr[l]:
-                    return True
-                l += 1
-            l = temp
+        
+        s = set()
+        for num in arr:
+            if (2*num in s) or (num % 2 == 0 and num // 2 in s):
+                return True
+            s.add(num)
         return False
