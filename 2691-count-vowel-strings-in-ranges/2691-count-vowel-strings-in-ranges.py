ae7fcc339f79e35fb08e92385c1fe899a026511d
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-01-07 11:57:26 +0900

    Time: 23 ms (92.58%), Space: 50 MB (60.42%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -5,18 +5,11 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        mem = []
-        s = 0 # prefix sum
+        n = len(words)
+        pre = [0] * (n + 1)
         vowels = set('aeiou')
-        for word in words:
-            if word[0] in vowels and word[-1] in vowels:
-                s += 1
-            mem.append(s)
-        print(mem)
-        res = []
-        for start, end in queries:
-            if start == 0:
-                res.append(mem[end])
-            else:
-                res.append(mem[end] - mem[start-1])
-        return res
+        for i in range(n):
+            pre[i + 1] = pre[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                pre[i + 1] += 1
+        return [pre[end + 1] - pre[start] for start, end in queries]
