69522dabf69cca0f4abf056857729dc92c2b6be5
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-02-05 10:28:18 +0900

    Time: 0 ms (100%), Space: 12.3 MB (88.28%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        cnt = 0
+        if s1 == s2:
+            return True
+
+        diff = []
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                cnt += 1
-        if cnt == 0 or cnt == 2:
-            return Counter(s1) == Counter(s2)
-        
+                diff.append(i)
+        if len(diff) == 2:
+            return s1[diff[0]] == s2[diff[1]] and s1[diff[1]] == s2[diff[0]]
+
         else:
             return False
