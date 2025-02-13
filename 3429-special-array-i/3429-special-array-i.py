3960f1e99d912ae0d2dc7f303a5278899d6df898
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-02-13 17:50:34 +0900

    Time: 2 ms (35.69%), Space: 12.5 MB (9.05%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(1, len(nums)):
+            if (nums[i - 1] & 1) == (nums[i] & 1):
+                return False
+        
+        return True
