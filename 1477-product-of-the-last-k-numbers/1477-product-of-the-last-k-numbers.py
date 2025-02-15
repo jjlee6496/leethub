827bf1b5db7b93c890e34c550c9e2e2aa2a5ed7d
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-02-15 16:29:14 +0900

    Time: 47 ms (82%), Space: 29.1 MB (46%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -2,8 +2,6 @@ class ProductOfNumbers(object):
 
     def __init__(self):
         self.mem = [1]
-        self.n = 1 # pointer of present pos
-        self.last_zero_ind = -1
 
     def add(self, num):
         """
@@ -11,27 +9,18 @@ class ProductOfNumbers(object):
         :rtype: None
         """
         if num == 0:
-            self.last_zero_ind = self.n
-        
-        if self.mem[-1] == 0:
-            self.mem.append(num)
+            self.mem = [1]
         else:
             self.mem.append(self.mem[-1] * num)
-        
-        self.n += 1
-        
 
     def getProduct(self, k):
         """
         :type k: int
         :rtype: int
         """
-        if self.last_zero_ind >= self.n - k:
+        if k >= len(self.mem):
             return 0
-        if self.mem[self.n - k - 1] == 0: # to prevent zero division
-            return self.mem[-1]
-        else:
-            return self.mem[-1] / self.mem[self.n - k - 1]
+        return self.mem[-1] / self.mem[-k - 1]
 
 # Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
