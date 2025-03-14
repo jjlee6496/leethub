f0e3491cb9d44f9ee0e361bfe010307bd1ec1ab8
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-03-14 16:54:41 +0900

    Time: 159 ms (100%), Space: 20.2 MB (48.89%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -1,19 +1,30 @@
 class Solution(object):
+    def check(self, candies, t, k):
+        cnt = 0
+        for candy in candies:
+            cnt += candy // t
+        if cnt >= k:
+            return True
+        else:
+            return False
+
     def maximumCandies(self, candies, k):
         """
         :type candies: List[int]
         :type k: int
         :rtype: int
         """
-        if sum(candies) < k:
+        s = sum(candies)
+        if s < k:
             return 0
 
-        l, r = 1, max(candies)
-        while l < r:
-            m = l + (r - l + 1) // 2
-            res = sum(pile // m for pile in candies)
-            if res >= k:
-                l = m
+        l, r = 1, s / k
+        ans = 0
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.check(candies, m, k):
+                ans = max(ans, m)
+                l = m + 1
             else:
                 r = m - 1
-        return l
+        return ans
