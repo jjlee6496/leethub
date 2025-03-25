52eacf32c878c4e0c5cc2c5d4a26054c68c22360
Jeong Jin Lee <96418287+jjlee6496@users.noreply.github.com>
2025-03-25 10:38:23 +0900

    Time: 733 ms (60%), Space: 70 MB (40%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def slicing(self, l):
+        cnt = 0
+        prev = l[0]
+        for i in range(1, len(l)):
+            if l[i][0] >= prev[1]:
+                cnt += 1
+                prev = l[i]
+            else:
+                prev = (prev[0], max(prev[1], l[i][1]))
+        return cnt
+
+    def checkValidCuts(self, n, rectangles):
+        """
+        :type n: int
+        :type rectangles: List[List[int]]
+        :rtype: bool
+        """
+        x = sorted([(r[0], r[2]) for r in rectangles])
+        y = sorted([(r[1], r[3]) for r in rectangles])
+
+        a = self.slicing(x)
+        b = self.slicing(y)        
+        return a >= 2 or b >= 2
